895b4e4fdc272f086eb9b7fc7bce4fdf96b425d5
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-27 18:35:41 -0500

    final

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,24 @@ import { useState } from "react";
 export default function Home() {
   const [victor, setVictor] = useState(pet);
 
+  
+
+  function handleClick(){
+    let clone = {...victor[0]};
+    clone.id = victor.length + 1;
+    setVictor([...victor, clone])
+  }
+
+  function handleChange(e){
+    const chosenID = +e.target.value;
+    console.log(chosenID);
+    setVictor(victor.filter((object)=>{
+     return (object.id !== chosenID)
+    }))
+  }
 
 
-  // <img src="/victor.png" alt="friendly dog" />
-
+  
   return (
     <main>
       <h1>Cloner</h1>
@@ -19,9 +33,35 @@ export default function Home() {
         You can use Victor or your own pet's image.<br/>
       </p>
 
-      
+      <div id="container">
+        {victor.map((goodBoy)=>{
+          return (
+              <div className="card" key={goodBoy.id}>
+                <h3>{goodBoy.petName}</h3>
+                <p>ID: {goodBoy.id}</p>
+                <img src="/victor.png" alt="friendly dog" />
+                <button onClick={handleClick}>Clone!</button>
+              </div>
+              )
+            }
+          )
+        }
+      </div>
 
       <h2>Use the filter method and a select box to remove the clones that didn't turn out right.</h2>
+
+      <label>SELECT CLONE TO TERMINATE:</label>
+      <select onChange={handleChange}>
+        {victor.map((goodBoy)=>{
+          return (
+              <option value={goodBoy.id} key={goodBoy.id}>
+                {goodBoy.id}
+              </option>
+              )
+            }
+          )
+        }     
+      </select>
     </main>
   );
 }
