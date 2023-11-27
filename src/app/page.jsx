c5011e0a2569ad19277bf49e8535994c6b47bc2b
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-27 16:27:23 -0500

    changed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,23 @@ import { useState } from "react";
 export default function Home() {
   const [victor, setVictor] = useState(pet);
 
-  
+
 
   // <img src="/victor.png" alt="friendly dog" />
 
   return (
     <main>
+      <h1>Cloner</h1>
+      <p>
+        You will store your clones as an array of objects.<br/>
+        You must utilize the Spread Operator to make your clones (...).<br/>
+        The only thing about your clone (each object) that changes is the id.<br/>
+        You can use Victor or your own pet's image.<br/>
+      </p>
+
+      
+
+      <h2>Use the filter method and a select box to remove the clones that didn't turn out right.</h2>
     </main>
   );
 }
